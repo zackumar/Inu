ff07fb7ff11314b6ae347bb52766384c939477ed
Zack Umar <zackumar2@gmail.com>
2022-02-13 22:28:07 -0600

    Added data struct

---
+++ code/Inu/src/controller.h
@@ -0,0 +1,10 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+typedef struct ControllerData {
+    float mode;
+    float position[3];
+    float rotation[3];
+} ControllerData;
+
+#endif
--- code/Inu/src/main.cpp
+++ code/Inu/src/main.cpp
@@ -1,62 +1,60 @@
 
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
+#include "controller.h"
 #include "kinematics.h"
 #include "servo_controller.h"
 
-int left_angles[3] = {90, 90, 90};
-int right_angles[3] = {90, 90, 90};
-float coords[3] = {0, 0, 0};
+SoftwareSerial ser(2, 3);
+
+ControllerData data;
 
 ServoController servo = ServoController();
 Kinematics kinematics = Kinematics();
 
-const int x_pot_pin = A0;
-const int y_pot_pin = A1;
-const int z_pot_pin = A2;
-int x_val = 0;
-int y_val = 0;
-int z_val = 0;
+float fl_coords[3] = {0.0, 0.0, 100.0};
+float fr_coords[3] = {0.0, 0.0, 100.0};
+
+int fl_angles[3] = {90, 90, 90};
+int fr_angles[3] = {90, 90, 90};
+
+char message[50];
 
 void setup() {
-    pinMode(x_pot_pin, INPUT);
-    pinMode(y_pot_pin, INPUT);
-    pinMode(z_pot_pin, INPUT);
-
-    Serial.begin(9600);
+    Serial.begin(115200, SERIAL_8N1);
+    ser.begin(115200);
 
     servo.start();
 
-    Serial.println("Inu Running...");
+    servo.move_servo(0, 90);
+    servo.move_servo(1, 90);
+    servo.move_servo(2, 90);
+    Serial.flush();
 }
 
 void loop() {
-    x_val = analogRead(x_pot_pin);
-    y_val = analogRead(y_pot_pin);
-    z_val = analogRead(z_pot_pin);
+    delay(10);
 
-    int x = map(x_val, 0, 1023, -150, 150);
-    int y = map(y_val, 0, 1023, -100, 100);
-    int z = map(z_val, 0, 1023, 1, 200);
+    if (Serial.available()) {
+        Serial.readBytes((byte*)&data, sizeof(ControllerData));
+    }
 
-    Serial.print(x);
-    Serial.print(",");
-    Serial.print(y);
-    Serial.print(",");
-    Serial.println(z);
+    if (data.mode == 1.0f) {
+        fl_coords[0] = data.position[0];
+        fl_coords[1] = data.position[1];
+        fl_coords[2] = data.position[2];
 
-    coords[0] = x;
-    coords[1] = y;
-    coords[2] = z;
+        kinematics.single_leg_ik(fl_angles, fl_coords, 0.0, 0.0, true);
 
-    kinematics.single_leg_ik(left_angles, coords, 0, 0, true);
-    kinematics.single_leg_ik(right_angles, coords, 0, 0, false);
+        servo.move_servo(0, fl_angles[0]);
+        servo.move_servo(1, fl_angles[1]);
+        servo.move_servo(2, fl_angles[2]);
 
-    servo.move_servo(0, left_angles[0]);
-    servo.move_servo(1, left_angles[1]);
-    servo.move_servo(2, left_angles[2]);
-
-    servo.move_servo(4, right_angles[0]);
-    servo.move_servo(5, right_angles[1]);
-    servo.move_servo(6, right_angles[2]);
+        ser.print(fl_angles[0]);
+        ser.print(" ");
+        ser.print(fl_angles[1]);
+        ser.print(" ");
+        ser.println(fl_angles[2]);
+    }
 }
