2923a7dd538ced1cff8a07bdf983d91b67fdc8de
Zack Umar <zackumar2@gmail.com>
2022-02-14 14:03:36 -0600

    Added other leg. Swtiched to timer instead of delay

--- code/Inu/src/main.cpp
+++ code/Inu/src/main.cpp
@@ -19,6 +19,10 @@ float fr_coords[3] = {0.0, 0.0, 100.0};
 int fl_angles[3] = {90, 90, 90};
 int fr_angles[3] = {90, 90, 90};
 
+unsigned long currentMillis;
+unsigned long prevMillis = 0;
+unsigned long interval = 10;
+
 char message[50];
 
 void setup() {
@@ -27,34 +31,51 @@ void setup() {
 
     servo.start();
 
-    servo.move_servo(0, 90);
-    servo.move_servo(1, 90);
-    servo.move_servo(2, 90);
+    servo.move_servo(0, fl_angles[0]);
+    servo.move_servo(1, fl_angles[1]);
+    servo.move_servo(2, fl_angles[2]);
+
+    servo.move_servo(4, fr_angles[0]);
+    servo.move_servo(5, fr_angles[1]);
+    servo.move_servo(6, fr_angles[2]);
+
     Serial.flush();
 }
 
 void loop() {
-    delay(10);
+    currentMillis = millis();
+    if (currentMillis - prevMillis >= interval) {
+        prevMillis = currentMillis;
 
-    if (Serial.available()) {
-        Serial.readBytes((byte*)&data, sizeof(ControllerData));
-    }
+        if (Serial.available()) {
+            Serial.readBytes((byte*)&data, sizeof(ControllerData));
+        }
 
-    if (data.mode == 1.0f) {
-        fl_coords[0] = data.position[0];
-        fl_coords[1] = data.position[1];
-        fl_coords[2] = data.position[2];
+        if (data.mode == 1.0) {
+            kinematics.single_leg_ik(fl_angles, data.position, 0.0, 0.0, true);
+            kinematics.single_leg_ik(fr_angles, data.position, 0.0, 0.0, false);
 
-        kinematics.single_leg_ik(fl_angles, fl_coords, 0.0, 0.0, true);
+            servo.move_servo(0, fl_angles[0]);
+            servo.move_servo(1, fl_angles[1]);
+            servo.move_servo(2, constrain(fl_angles[2], 0, 170));
 
-        servo.move_servo(0, fl_angles[0]);
-        servo.move_servo(1, fl_angles[1]);
-        servo.move_servo(2, fl_angles[2]);
+            servo.move_servo(4, fr_angles[0]);
+            servo.move_servo(5, fr_angles[1]);
+            servo.move_servo(6, constrain(fr_angles[2], 10, 180));
 
-        ser.print(fl_angles[0]);
-        ser.print(" ");
-        ser.print(fl_angles[1]);
-        ser.print(" ");
-        ser.println(fl_angles[2]);
+            ser.print("Left - W: ");
+            ser.print(fl_angles[0]);
+            ser.print(" E: ");
+            ser.print(fl_angles[1]);
+            ser.print(" S: ");
+            ser.print(fl_angles[2]);
+            // ser.print(" | Right - W: ");
+            // ser.print(fr_angles[0]);
+            // ser.print(" E: ");
+            // ser.print(fr_angles[1]);
+            // ser.print(" S: ");
+            // ser.print(fr_angles[2]);
+            ser.println();
+        }
     }
 }
