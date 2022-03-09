5e8fb8b4296168eb263683c3094608f67924b49d
Zack Umar <zackumar2@gmail.com>
2022-03-09 10:32:35 -0600

    Add Rotation

--- code/Controller/src/main.py
+++ code/Controller/src/main.py
@@ -19,6 +19,7 @@ arduino = serial.Serial(
 )
 
 mode = 0
+rotation_trigger = False
 
 # Min is one more than actual for vmap
 x_min = -149
@@ -27,6 +28,8 @@ y_min = -99
 y_max = 100
 z_min = 2
 z_max = 200
+
+
 
 struct_format = 'fffffff'
 struct_size = struct.calcsize(struct_format)
@@ -37,7 +40,13 @@ def main():
     x = 0
     y = 0
     z = 100 # Resting pos
+
+    roll = 0
+    pitch = 0
+    yaw = 0
+
     global mode
+    global rotation_trigger
 
     previous = None
 
@@ -60,25 +69,48 @@ def main():
         #     print(message)
 
         for event in pygame.event.get():
+
             if event.type == pygame.JOYBUTTONDOWN:
                 if(event.button == 7):
                     x = 0
                     y = 0
                     z = 141
 
+                    pitch = 0
+                    roll = 0
+                    yaw = 0
+
+                if(event.button == 4):
+                    rotation_trigger = True
+
+            if event.type == pygame.JOYBUTTONUP:
+                if(event.button == 4):
+                    rotation_trigger = False                
 
 
             if event.type == pygame.JOYAXISMOTION:
-                if event.axis == 1:
-                   x = vmap(event.value, -1, 1, x_min, x_max)
-                   if(x < 10 and x > -10): x = 0
-                if event.axis == 0:
-                    y = vmap(event.value, -1, 1, y_max, y_min)
-                    if(y < 10 and y > -10): y = 0
+                if not rotation_trigger:
+                    if event.axis == 1:
+                        x = vmap(event.value, -1, 1, x_min, x_max)
+                        if(x < 10 and x > -10): x = 0
+                    if event.axis == 0:
+                        y = vmap(event.value, -1, 1, y_max, y_min)
+                        if(y < 10 and y > -10): y = 0
 
-                if(event.axis == 3):
-                    z = vmap(event.value, -1, 1, z_max, z_min)
-                    if(z < 105 and z > 95): z = 100
+                    if(event.axis == 3):
+                        z = vmap(event.value, -1, 1, z_max, z_min)
+                        if(z < 105 and z > 95): z = 100
+                else:
+                    if event.axis == 1:
+                        roll = vmap(event.value, -1, 1, -90, 90)
+                        if(x < 10 and x > -10): x = 0
+                    if event.axis == 0:
+                        pitch = vmap(event.value, -1, 1, 90, -90)
+                        if(y < 10 and y > -10): y = 0
+
+                    if(event.axis == 2):
+                        yaw = vmap(event.value, -1, 1, -90, 90)
+                        if(z < 10 and z > -10): z = 0
 
             if event.type == pygame.JOYHATMOTION:
                 hat_x, hat_y = event.value
@@ -88,11 +120,11 @@ def main():
                 if(hat_y == -1 and mode >0):
                     mode -= 1 
         
+        botdata = struct.pack(struct_format,mode, x, y, z, roll, pitch, yaw)
 
-        botdata = struct.pack(struct_format,mode, x, y, z, 0.0, 0.0, 0.0)
 
         if(botdata != previous):
-            print(mode, x, y, z, 0.0, 0.0, 0.0)
+            print(mode, x, y, z, roll, pitch, yaw)
 
             arduino.write(botdata)
             previous = botdata
